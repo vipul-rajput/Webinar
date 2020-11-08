bdabe789505817dcac5a141182c9464c9586a4f4
Vipul <vipulpr88@gmail.com>
2020-11-08 22:36:34 +0530

    Minute Changes

--- myresume/settings.py
+++ myresume/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'e!n048o&x^z%zovm%w03)!8r+)n)+43d14*s@@d*11wnjk*^(z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'vipulprofile.herokuapp.com']
 
